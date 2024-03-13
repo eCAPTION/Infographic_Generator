84af1fe8ab287e580723a1bfc943bcb9e23c2117
zhuoyang125 <zhuoyang125@gmail.com>
2024-03-14 01:12:59 +0800

    update kafka functions with s3

--- kafka_app/kafka_handler.py
+++ kafka_app/kafka_handler.py
@@ -1,16 +1,53 @@
 import os
-import requests
+import json
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 from PIL import Image
+from io import BytesIO
 
-from ecaption_utils.kafka.faust import get_faust_app, initialize_topics, FaustApplication
+from ecaption_utils.kafka.faust import get_faust_app, initialize_topics, FaustApplication, get_error_handler
 from ecaption_utils.kafka.topics import Topic, get_event_type
 
 from util import *
 
-NUM_LABEL = 5
-GENERATION_ENDPOINT = 'https://infographic-generator-106858723129.herokuapp.com'
+'''
+metadata for each infographic
+{
+label: list[int],
+input_dict: dict[int, list[tuple(str, str)]] (component_name, content)
+bbox: list[list[int]],
+request_id: int,
+title: str,
+desc: str,
+related_articles: str,
+image: str,
+adj_list: dict[int, list[int]],
+node_occurences: dict[int, int],
+entity_labels = dict[int, str]
+}
+'''
 
+# Constants
+NUM_LABEL = 5
+CANVAS_HEIGHT, CANVAS_WIDTH = 297, 210
+GENERATION_ENDPOINT = 'https://infographic-generator-106858723129.herokuapp.com'
+INFOGRAPHIC_URL = 'https://generated-infographics.s3.ap-southeast-1.amazonaws.com'
+BUCKET_NAME = 'generated-infographics'
+
+# component_id_mapping = {
+#     'title': 0,
+#     'header': 1,
+#     'related_articles': 2,
+#     'image': 3,
+#     'knowledge_graph': 4
+# }
+component_label_mapping = {
+    'title': 0,
+    'header': 0,
+    'related_articles': 0,
+    'image': 1,
+    'knowledge_graph': 2
+}
 load_dotenv()
 broker_url = os.environ.get("KAFKA_BROKER_URL")
 port = os.environ.get("GATEWAY_SERVICE_PORT")
@@ -19,6 +56,8 @@ bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
 app = get_faust_app(FaustApplication.Chatbot, broker_url=broker_url, port=port)
 topics = initialize_topics(app, [Topic.NEWS_SEARCH_RESULTS, Topic.ADD_INSTRUCTION, Topic.DELETE_INSTRUCTION, Topic.NEW_INFOGRAPHIC, Topic.MODIFIED_INFOGRAPHIC])
 
+handle_error = get_error_handler(app)
+
 @app.agent(topics[Topic.NEWS_SEARCH_RESULTS])
 async def handle_infographic_generation(event_stream):
     async for event in event_stream:
@@ -26,14 +65,11 @@ async def handle_infographic_generation(event_stream):
         title = event.title
         desc = event.description
         related_articles = event.related_articles
-        related_facts = event.related_facts
-
-        texts = [title, desc, related_articles, related_facts]
+        texts = [('title', title), ('description', desc), ('related_articles', related_articles)]
 
         img_url = event.image
-        # image
         im = Image.open(img_url)
-        imgs = [im]
+        imgs = [('image', im)]
 
         # knowledge subgraph information
         adj_list = event.adjList
@@ -41,23 +77,54 @@ async def handle_infographic_generation(event_stream):
         entity_labels = event.entity_labels
 
         graph_im = convert_graph_to_image(adj_list, node_occurences, entity_labels) # im is a Pillow Image object
-        graphs = [graph_im]
+        graphs = [('knowledge_graph', graph_im)]
 
         # do the infographic generation here to obtain img url
         label = []
-        for _ in texts:
-            label.append(0)
-        for _ in imgs:
-            label.append(1)
-        for _ in graphs:
-            label.append(2)
+        input_dict = {0: texts, 1: imgs, 2: graphs}
+        for k in input_dict:
+            for _ in range(len(input_dict[k])):
+                label.append(k)
+
+        try:
+            gen_bbox, gen_label = get_generation_from_api(NUM_LABEL, label)
+        except Exception as e:
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while access infographic generation API: ' + str(e)
+            )
+            continue
+
+        # stores information relevant to this infographic layout
+        layout_dict = event_to_dict(event) 
+        layout_dict['bbox'] = gen_bbox
+        layout_dict['label'] = gen_label
+        layout_dict['input_dict'] = input_dict
+        infographic_img = convert_layout_to_infographic(input_dict, gen_bbox, gen_label, (CANVAS_HEIGHT, CANVAS_WIDTH))
         
-        gen_bbox, gen_label = get_generation_from_api(NUM_LABEL, label)
-        # send the url back 
-        DUMMY_URL = 'https://assets1.cbsnewsstatic.com/hub/i/r/2011/04/13/0c9bfbe6-a643-11e2-a3f0-029118418759/thumbnail/1200x630/83017e8b1510dc4907cf08bc9f0cc814/Biden_1.png?v=b15ba9ca383d8472a236fb5b5c7ca2b3' # TODO: get the actual URL
+        # save image and layout data to stream
+        img_bytes = BytesIO()
+        infographic_img.save(img_bytes, format='JPEG')
+        img_bytes.seek(0)
+        json_layout_data = json.dumps(layout_dict)
+        json_layout_bytes = BytesIO(json_layout_data.encode('utf-8'))
+        
+        # upload stream to s3 bucket
+        img_success = upload_fileobj(img_bytes, BUCKET_NAME, '{}.jpeg'.format(str(request_id)))
+        json_success = upload_fileobj(json_layout_bytes, BUCKET_NAME, '{}.json'.format(str(request_id)))
+        if not (img_success and json_success):
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while uploading to S3: ' + str(e)
+            )
+            continue
+        # send the url back
+        url = INFOGRAPHIC_URL + '/' + str(request_id)
         topic = Topic.NEW_INFOGRAPHIC
         Event = get_event_type(topic)
-        await topics[topic].send(infographic_link=DUMMY_URL, request_id=request_id)
+        await topics[topic].send(infographic_link=url, request_id=str(request_id))
 
 @app.agent(topics[Topic.DELETE_INSTRUCTION])
 async def handle_delete_instruction(event_stream):
@@ -66,31 +133,153 @@ async def handle_delete_instruction(event_stream):
         infographic_link = event.infographic_link
         infographic_section = event.infographic_section
 
-        # do the infographic generation here to obtain img url
+        layout_object_name = infographic_link.split('/')[1] + '.json'
+        json_file_in_mem = BytesIO() 
 
-        # send the url back 
-        DUMMY_URL = 'https://assets1.cbsnewsstatic.com/hub/i/r/2011/04/13/0c9bfbe6-a643-11e2-a3f0-029118418759/thumbnail/1200x630/83017e8b1510dc4907cf08bc9f0cc814/Biden_1.png?v=b15ba9ca383d8472a236fb5b5c7ca2b3' # TODO: get the actual URL
-        topic = Topic.NEW_INFOGRAPHIC
+        # do the infographic generation here to obtain img url
+        download_fileobj(BUCKET_NAME, layout_object_name, json_file_in_mem)
+        json_file_in_mem.seek(0)
+
+        downloaded_json_data = json_file_in_mem.read()
+        layout_dict = json.loads(downloaded_json_data)
+        label = layout_dict['label'] 
+        input_dict = {}
+        for k in layout_dict['input_dict']:
+            input_dict[int(k)] = layout_dict['input_dict'][k]
+
+        # remove infographic section from input dict
+        label_of_delete_section = component_label_mapping[infographic_section]
+        tup_dict = dict(input_dict[label_of_delete_section])
+        tup_dict.pop(infographic_section)
+        input_dict[label_of_delete_section] = list(tuple(tup_dict.items()))
+
+        # update label after removing section
+        label = []
+        for k in layout_dict:
+            for _ in input_dict[k]:
+                label.append(k)
+
+
+        # get new layout
+        try:
+            gen_bbox, gen_label = get_generation_from_api(NUM_LABEL, label)
+        except Exception as e:
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while access infographic generation API: ' + str(e)
+            )
+            continue
+
+        infographic_img = convert_layout_to_infographic(input_dict, gen_bbox, gen_label, (CANVAS_HEIGHT, CANVAS_WIDTH))
+        # update layout dict
+        layout_dict['label'] = label
+        layout_dict['input_dict'] = input_dict
+        layout_dict['bbox'] = gen_bbox
+
+        # save image data to stream
+        img_bytes = BytesIO()
+        infographic_img.save(img_bytes, format='JPEG')
+        img_bytes.seek(0)
+        json_layout_data = json.dumps(layout_dict)
+        json_layout_bytes = BytesIO(json_layout_data.encode('utf-8'))
+        # upload stream to s3 bucket
+        img_success = upload_fileobj(img_bytes, BUCKET_NAME, '{}.jpeg'.format(str(request_id)))
+        json_success = upload_fileobj(json_layout_bytes, BUCKET_NAME, '{}.json'.format(str(request_id)))
+        if not (img_success and json_success):
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while uploading to S3: ' + str(e)
+            )
+            continue
+        # send the url back
+        url = INFOGRAPHIC_URL + '/' + str(request_id)
+        topic = Topic.MODIFIED_INFOGRAPHIC
         Event = get_event_type(topic)
-        await topics[topic].send(infographic_link=DUMMY_URL)
+        await topics[topic].send(infographic_link=url, request_id=str(request_id))
+
         
-@app.agent(topics[Topic.DELETE_INSTRUCTION])
+@app.agent(topics[Topic.ADD_INSTRUCTION])
 async def handle_add_instruction(event_stream):
     async for event in event_stream:
         request_id = event.request_id
         infographic_link = event.infographic_link
         target_element = event.target_element
-        infographic_section = event.infographic_section
         
-        # add a target element next to (right or left) existing infographic section
+        layout_object_name = infographic_link.split('/')[1] + '.json'
+        json_file_in_mem = BytesIO() 
 
-        # send the url back 
-        DUMMY_URL = 'https://assets1.cbsnewsstatic.com/hub/i/r/2011/04/13/0c9bfbe6-a643-11e2-a3f0-029118418759/thumbnail/1200x630/83017e8b1510dc4907cf08bc9f0cc814/Biden_1.png?v=b15ba9ca383d8472a236fb5b5c7ca2b3' # TODO: get the actual URL
-        topic = Topic.NEW_INFOGRAPHIC
+        # do the infographic generation here to obtain img url
+
+        # get metadata of existing layout
+        download_fileobj(BUCKET_NAME, layout_object_name, json_file_in_mem)
+        json_file_in_mem.seek(0)
+
+        downloaded_json_data = json_file_in_mem.read()
+        layout_dict = json.loads(downloaded_json_data)
+        input_dict = {}
+        for k in layout_dict['input_dict']:
+            input_dict[int(k)] = layout_dict['input_dict'][k]
+        
+        
+        # check if target element already exists
+        target_element_exists = False
+        for k in input_dict:
+            for v in input_dict[k]:
+                if v[0] == target_element:
+                    target_element_exists = True
+        if target_element_exists:
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Target element already exists'
+            )
+        
+        target_element_label = component_label_mapping[target_element]
+        input_dict[target_element_label].append((target_element, layout_dict[target_element]))
+        # update label after adding target element
+        label = []
+        for k in layout_dict:
+            for _ in input_dict[k]:
+                label.append(k)
+        # get new layout
+        try:
+            gen_bbox, gen_label = get_generation_from_api(NUM_LABEL, label)
+        except Exception as e:
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while access infographic generation API: ' + str(e)
+            )
+            continue
+
+        infographic_img = convert_layout_to_infographic(input_dict, gen_bbox, gen_label, (CANVAS_HEIGHT, CANVAS_WIDTH))
+        # update layout dict
+        layout_dict['label'] = label
+        layout_dict['input_dict'] = input_dict
+        layout_dict['bbox'] = gen_bbox
+
+        # save image data to stream
+        img_bytes = BytesIO()
+        infographic_img.save(img_bytes, format='JPEG')
+        img_bytes.seek(0)
+        json_layout_data = json.dumps(layout_dict)
+        json_layout_bytes = BytesIO(json_layout_data.encode('utf-8'))
+
+        # upload stream to s3 bucket
+        img_success = upload_fileobj(img_bytes, BUCKET_NAME, '{}.jpeg'.format(str(request_id)))
+        json_success = upload_fileobj(json_layout_bytes, BUCKET_NAME, '{}.json'.format(str(request_id)))
+        if not (img_success and json_success):
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.InfographicGeneration,
+                error_message='Error while uploading to S3: ' + str(e)
+            )
+            continue
+        # send the url back
+        url = INFOGRAPHIC_URL + '/' + str(request_id)
+        topic = Topic.MODIFIED_INFOGRAPHIC
         Event = get_event_type(topic)
-        await topics[topic].send(infographic_link=DUMMY_URL)
-
-
-
-
+        await topics[topic].send(infographic_link=url, request_id=str(request_id))
         
--- kafka_app/util.py
+++ kafka_app/util.py
@@ -3,6 +3,9 @@ import requests
 import matplotlib.pyplot as plt
 from PIL import Image
 import json
+import boto3
+from botocore.exceptions import ClientError
+import logging
 
 GENERATION_ENDPOINT = 'https://infographic-generator-106858723129.herokuapp.com'
 
@@ -32,8 +35,119 @@ def convert_graph_to_image(adj_list, node_occurences, entity_labels):
     img = convert_plt_to_img(fig)
     return img
 
+def event_to_dict(event):
+    '''
+    creates and returns a dict containing all event information.
+    '''
+    return {
+        'request_id': event.request_id,
+        'title': event.title,
+        'description': event.description,
+        'related_articles': event.related_articles,
+        'image': event.image,
+        'adj_list': event.adjList,
+        'node_occurences': event.node_occurences,
+        'entity_labels': event.entity_labels
+    }
+
+# Querying infographic generator endpoint
 def get_generation_from_api(num_label, label):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     res = requests.post(url=GENERATION_ENDPOINT + '/generate', data=json.dumps({'num_label': num_label, 'label': label}), headers=headers, timeout=60)
     bboxes, labels = res.json()['results']['bbox'], res.json()['results']['label']
     return bboxes, labels
+
+def get_edit_from_api(id_a, id_b, relation, bbox, num_label, label):
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    res = requests.post(url=GENERATION_ENDPOINT + '/generate', data=json.dumps({'id_a': id_a, 'id_b': id_b, 'relation': relation, 'bbox': bbox, 'num_label': num_label, 'label': label}), headers=headers, timeout=60)
+    bboxes, labels = res.json()['results']['bbox'], res.json()['results']['label']
+    return bboxes, labels
+
+def convert_layout_to_infographic(input_dict, boxes, labels, canvas_size): 
+    '''
+    the input is a dict[label_index: [values of each label element]]
+    e.g. {0: ['Biden', 'Trump']}
+    images are represented by Pillow Image object.
+    '''
+    H, W = canvas_size
+    img = Image.new('RGB', (int(W), int(H)), color=(255,255,255))
+    draw = ImageDraw.Draw(img, 'RGBA')
+
+    area = [b[2] * b[3] for b in boxes]
+    indices = sorted(range(len(area)), key=lambda i:area[i], reverse=True)
+
+    for i in indices:
+        bbox, label = boxes[i], labels[i]
+        x1, y1, x2, y2 = convert_xywh_to_ltrb(bbox)
+        x1, y1, x2, y2 = int(x1*W), int(y1*H), int(x2*W), int(y2*H)
+
+        if label == 1 or label == 2:
+            img_to_paste = input_dict[label][0].resize((x2-x1, y2-y1))
+            img.paste(img_to_paste, (x1, y1, x2, y2))
+            input_dict[label].pop(0)
+        else:
+            # text
+            text = input_dict[label][0]
+            font_size = 100
+            words = text.split()
+            
+            while font_size > 0:
+                size = None
+                curr_words = []
+                idx = 0 # index of current word to select
+                while idx < len(words):
+                    l, t, r, b = draw.multiline_textbbox((x1, y1), ' '.join(curr_words), font_size=font_size)
+                    size = (r-l, b-t) # (width, height)
+                    if size[1] > y2-y1:
+                        break
+                    if size[0] < x2-x1:
+                        curr_words.append(words[idx])
+                        idx += 1
+                    else:
+                        if curr_words[-1] == '\n':
+                            break
+                        curr_words.pop()
+                        idx -= 1
+                        curr_words.append('\n') # add new line  
+
+                if idx >= len(words):
+                    break
+                font_size -= 1
+            draw.multiline_text((x1, y1), ' '.join(curr_words), fill="#000", font_size=font_size)
+    return img
+
+# AWS Operations
+def upload_fileobj(file_object, bucket, object_name):
+    """Upload a file to an S3 bucket
+
+    :param file_obj: File object to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_fileobj(file_object, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+def download_fileobj(bucket, object_name, file_object):
+    """
+    Download a file from S3 bucket
+
+    :param file_name: file name to download
+    :param bucket: bucket to download from
+    :param object_name S3 object name
+    :return True if file was succesfully downloaded, else False
+    """
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.download_fileobj(bucket, object_name, file_object)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
